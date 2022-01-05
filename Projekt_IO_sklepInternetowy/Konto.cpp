2e6fc05d72f709efe04a3fa7f48a259fcf329a2c
RobertZ00 <zachwiejar@student.agh.edu.pl>
2022-01-05 12:54:02 +0100

    update konto

--- Projekt_IO_sklepInternetowy/Konto.cpp
+++ Projekt_IO_sklepInternetowy/Konto.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 Konto::Konto(string imieKlienta, string nazwiskoKlienta, Klient id)
 {
-
+	Konto::imie = imieKlienta;
+	Konto::nazwisko = nazwiskoKlienta;
+	Konto::idKlienta = id;
 }
