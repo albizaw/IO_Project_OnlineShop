2c48d7765a64c18d85e03f7b42ea433b29ab4de1
Albert <zawadaa10@gmail.com>
2022-01-05 11:57:03 +0100

    proste menu

--- Projekt_IO_sklepInternetowy/Źródło.cpp
+++ Projekt_IO_sklepInternetowy/Źródło.cpp
@@ -8,6 +8,33 @@ int main()
 
 	cout << "Witaj w naszym sklepie internetowym! " << endl;
 	
-	cout << 
+	int wybor = 0;
+
+	while(wybor > 3 || wybor < 1)
+	{ 
+		cout << "MENU" << endl;
+		cout << "1.Administrator" << endl;
+		cout << "2.Sprzedawca" << endl;
+		cout << "3.Klient" << endl;
+
+		cout << "Wybierz opcje 1 - 3: ";
+		cin >> wybor;
+		system("cls");
+		
+	}
+
+	switch (wybor)
+	{
+	case 1:
+	// to co robi administrator
+		break;
+
+	case 2:
+		// to co robi sprzedawca
+
+	case 3:
+		// to co robi klient
+	}
+
 
 }
