0726ff457128263b7fd093fac26afc955c492831
RobertZ00 <zachwiejar@student.agh.edu.pl>
2022-01-05 12:44:32 +0100

    zmiany konto i klient

--- Projekt_IO_sklepInternetowy/Konto.cpp
+++ Projekt_IO_sklepInternetowy/Konto.cpp
@@ -1,1 +1,9 @@
 #include "Konto.h"
+#include <string>
+
+using namespace std;
+
+Konto::Konto(string imieKlienta, string nazwiskoKlienta, Klient id)
+{
+
+}
--- Projekt_IO_sklepInternetowy/Konto.h
+++ Projekt_IO_sklepInternetowy/Konto.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <string>
+#include "Klient.h"
+
+using namespace std;
 
 class Konto {
 private:
 	string imie;
 	string nazwisko;
-	Klient id_klienta;
+	Klient idKlienta;
 public:
-
+	Konto(string imieKlienta, string nazwiskoKlienta, Klient id);
 };
